cdbe35fd468805bf9fce89779913c7fb93d8c584
lary song <cnre@live.cn>
2023-08-04 05:26:40 +0000

    chore: init

---
+++ raw/hack-ddxs.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Hack DYXS
+// @version      0.1
+// @author       You
+// @match        https://dyxs15.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.error('hack ddxs')
+    var style = document.createElement('style');
+    style.innerHTML = `
+        body > a#dy_card_dy {display: none}
+        .player-rm.rm-list {display:none}
+        .player_pic_link {pointer-events: none!important; display: none;}
+        .player_pic_box {
+            min-height: 240px;
+            min-width: 360px;
+            background: darkgray; display:grid;place-items:center}
+        .player_pic_box::before {
+            display: block;
+            content: 'AD';
+            color: lightgray;
+            font-size: 8em;
+        }
+    `;
+    document.head.appendChild(style);
+})();
